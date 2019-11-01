726d33014925a76c12dd3405822a8cf8104bcdd5
giulio <g.98.machado@hotmail.com>
2019-11-01 20:47:12 -0300

    iniciando criação de partidas e rodadas.
    obs.: ainda não está funcional: loop infinito

--- TrabalhoES2/TrabalhoES2/Form1.Designer.cs
+++ TrabalhoES2/TrabalhoES2/Form1.Designer.cs
@@ -28,13 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lb_times = new System.Windows.Forms.Label();
+            this.lb_arbitros = new System.Windows.Forms.Label();
+            this.lb_locais = new System.Windows.Forms.Label();
+            this.lb_atletas = new System.Windows.Forms.Label();
+            this.btn_novoAtleta = new System.Windows.Forms.Button();
+            this.btn_excluirAtleta = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb_times
+            // 
+            this.lb_times.AutoSize = true;
+            this.lb_times.Location = new System.Drawing.Point(126, 40);
+            this.lb_times.Name = "lb_times";
+            this.lb_times.Size = new System.Drawing.Size(35, 13);
+            this.lb_times.TabIndex = 0;
+            this.lb_times.Text = "Times";
+            // 
+            // lb_arbitros
+            // 
+            this.lb_arbitros.AutoSize = true;
+            this.lb_arbitros.Location = new System.Drawing.Point(287, 40);
+            this.lb_arbitros.Name = "lb_arbitros";
+            this.lb_arbitros.Size = new System.Drawing.Size(42, 13);
+            this.lb_arbitros.TabIndex = 2;
+            this.lb_arbitros.Text = "Árbitros";
+            // 
+            // lb_locais
+            // 
+            this.lb_locais.AutoSize = true;
+            this.lb_locais.Location = new System.Drawing.Point(201, 40);
+            this.lb_locais.Name = "lb_locais";
+            this.lb_locais.Size = new System.Drawing.Size(38, 13);
+            this.lb_locais.TabIndex = 3;
+            this.lb_locais.Text = "Locais";
+            // 
+            // lb_atletas
+            // 
+            this.lb_atletas.AutoSize = true;
+            this.lb_atletas.Location = new System.Drawing.Point(36, 40);
+            this.lb_atletas.Name = "lb_atletas";
+            this.lb_atletas.Size = new System.Drawing.Size(39, 13);
+            this.lb_atletas.TabIndex = 4;
+            this.lb_atletas.Text = "Atletas";
+            // 
+            // btn_novoAtleta
+            // 
+            this.btn_novoAtleta.Location = new System.Drawing.Point(12, 67);
+            this.btn_novoAtleta.Name = "btn_novoAtleta";
+            this.btn_novoAtleta.Size = new System.Drawing.Size(84, 23);
+            this.btn_novoAtleta.TabIndex = 5;
+            this.btn_novoAtleta.Text = "Novo Atleta";
+            this.btn_novoAtleta.UseVisualStyleBackColor = true;
+            // 
+            // btn_excluirAtleta
+            // 
+            this.btn_excluirAtleta.Location = new System.Drawing.Point(12, 96);
+            this.btn_excluirAtleta.Name = "btn_excluirAtleta";
+            this.btn_excluirAtleta.Size = new System.Drawing.Size(84, 23);
+            this.btn_excluirAtleta.TabIndex = 6;
+            this.btn_excluirAtleta.Text = "Excluir Atleta";
+            this.btn_excluirAtleta.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Excluir Atleta";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(116, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Novo Atleta";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(226, 96);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Excluir Atleta";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(226, 67);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Novo Atleta";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(329, 96);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Excluir Atleta";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(329, 67);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(84, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Novo Atleta";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_excluirAtleta);
+            this.Controls.Add(this.btn_novoAtleta);
+            this.Controls.Add(this.lb_atletas);
+            this.Controls.Add(this.lb_locais);
+            this.Controls.Add(this.lb_arbitros);
+            this.Controls.Add(this.lb_times);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lb_times;
+        private System.Windows.Forms.Label lb_arbitros;
+        private System.Windows.Forms.Label lb_locais;
+        private System.Windows.Forms.Label lb_atletas;
+        private System.Windows.Forms.Button btn_novoAtleta;
+        private System.Windows.Forms.Button btn_excluirAtleta;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- TrabalhoES2/TrabalhoES2/Program.cs
+++ TrabalhoES2/TrabalhoES2/Program.cs
@@ -17,6 +17,117 @@ namespace TrabalhoES2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            var clubes = new List<Time>();
+            for (int i = 0; i < 20; i++)
+            {
+                clubes.Add(new Time($"Clube{i+1}"));
+            }
+
+            var campeonato = new Campeonato();
+
+            var count = 0;
+
+            while(campeonato.Rodadas.Count < 19)
+            {
+                var rodada = new Rodada();
+
+                while(rodada.Partidas.Count < 10)
+                {
+                    foreach (var timeA in clubes)
+                    {
+                        foreach (var timeB in clubes)
+                        {
+                            if (!campeonato.JaOuveConfrontoEntreOsDoisTimes(timeA, timeB))
+                            {
+                                rodada.CriarPartida(timeA, timeB);
+                            }
+                        }
+                    }
+                }
+                campeonato.Rodadas.Add(rodada);
+            }
         }
     }
+
+    public class Campeonato
+    {
+        public List<Rodada> Rodadas { get; set; }
+        public Campeonato()
+        {
+            Rodadas = new List<Rodada>();
+        }
+
+        public bool JaOuveConfrontoEntreOsDoisTimes(Time timeA, Time timeB)
+        {
+            if (Rodadas.Count > 19)
+                return true;
+
+            foreach (var rodada in Rodadas)
+	        {
+                if (rodada.Partidas.Any(p => (p.TimeA == timeA && p.TimeB == timeB) || 
+                                             (p.TimeA == timeB && p.TimeB == timeA)))
+                {
+                    return true;
+                }
+	        }
+
+            return false;
+        }
+
+    }
+
+
+    public class Rodada
+    {
+        public List<Partida> Partidas { get; set; }
+        public Rodada()
+        {
+            Partidas = new List<Partida>();
+        }   
+
+        public void CriarPartida(Time timeA, Time timeB)
+        {
+            if (!TimeJaEstaJogandoNaRodada(timeA, timeB))
+            {
+                Partidas.Add(new Partida(timeA, timeB));
+            }
+        }
+
+        private bool TimeJaEstaJogandoNaRodada(Time timeA, Time timeB)
+        {
+            if (Partidas.Count >= 10)
+                return true;
+
+            if (timeA == timeB)
+                return true;
+
+            if (Partidas.Any(p => p.TimeA == timeA || p.TimeA == timeB || p.TimeB == timeA || p.TimeB == timeB))
+                return true;
+
+            return false;
+        }
+    }
+
+    public class Partida
+    {
+        public Time TimeA { get; set; }
+        public Time TimeB { get; set; }
+
+        public Partida(Time timeA, Time timeB)
+        {
+            TimeA = timeA;
+            TimeB = timeB;
+        }
+    }
+
+    public class Time
+    {
+        private string Nome { get; set; }
+        public Time(string nome)
+        {
+            this.Nome = nome;
+        }
+    }
+
 }
